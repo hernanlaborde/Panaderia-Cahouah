0d41e0acbe36e0c92c01f79a88ef718d2445137b
Rodinson Parra <rodinzon@live.com>
2017-05-13 11:35:46 -0500

    Eliminadas imagenes en hd

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Panaderia Cahouah | El mejor pan frances en Bogota</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="panaderia Cahouah, panaderia,pan,frances,colombia,Cahouah,bogota">
+	<meta name="description" content="Panaderia Cahouah | El mejor pan frances en bogota, nos especializamos en Croissants y Baguettes sin conservantes ni saborizantes">
+	<meta name="author" content="Panaderia Cahouah">
+   <link rel="shortcut icon" href="img/favicon.png">
+   <link rel="stylesheet" href="css/estilos.css">
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300|Roboto" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+	
+	<header>
+		<div class="container">
+			<nav>
+				<a href=""><i class="fa fa-home" aria-hidden="true"></i> Inicio</a>
+				<a href=""><i class="fa fa-coffee" aria-hidden="true"></i> La panadería</a>
+				<a href=""><i class="fa fa-shopping-basket" aria-hidden="true"></i> Productos</a>
+				<a href=""><i class="fa fa-cart-plus" aria-hidden="true"></i> Pedidos</a>
+				<a href=""><i class="fa fa-comments-o" aria-hidden="true"></i> Contacto</a>
+			</nav>
+		</div>
+	</header>
+
+
+
+	<section id="portada">
+		<div class="container">
+			<div class="textos-portada">
+			<h1>Cahouah</h1>
+			<h2>El mejor pan frances en bogota</h2>
+			<div class="boton">
+				<a href="">Ver Productos</a>
+				<a href="">Hacer Pedido</a>
+			</div>
+			</div>
+		</div>
+	</section>
+
+	
+	<section class="contenido-portada">
+		<div class="container">
+			<h1>Un poco de nosotros</h1>
+			<p>La panadería "Cahouah" es una pequeña panadería de barrio, ubicada en la ciudad de Bogotá en el barrio Victoria Norte, los datos de contacto son: 3194110335 y 6317452, sus propietarios son  Kelly y Felipe quienes iniciaron este negocio hace aproximadamente un año, la panadería pertenece y se puede incluir en el sector de los alimentos.</p>
+			<p>Kelly es francesa y es la maestra panadera, ofrecen panadería francesa artesanal, sus productos se destacan por no tener conservantes ni saborizantes. Sus principales líneas de productos son los  Croissants y Baguettes que quienes los prueban quedan encantados. <a href="" class="more">Leer mas...</a></p>
+		</div>
+	</section>
+
+
+
+	<section class="cajas">
+		<div class="container">
+			<div class="cajas-interno">
+				<a href=""><i class="fa fa-shopping-basket" aria-hidden="true"></i> Productos</a>
+				<a href=""><i class="fa fa-cart-plus" aria-hidden="true"></i> Pedidos</a>
+				<a href=""><i class="fa fa-comments-o" aria-hidden="true"></i> Contacto</a>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<p>Panaderia Cahouah &copy; 2017 - Carrera 12 N0. 12-34 - Celular: 3194110335 - Bogotá / Colombia</p>
+		</div>
+	</footer>
+	
+	<script type="text/javascript" src="js/scripts.js"></script>
+	
+	<!--[if lt IE 9]>
+	<script src=https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js></script>
+	<script src=https://oss.maxcdn.com/respond/1.4.2/respond.min.js></script>
+	<![endif]-->
+</body>
+</html>
