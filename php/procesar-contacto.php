a99d32819a6a88cad772b775a797cb3ea050ba93
Rodinson Parra <rodinzon@live.com>
2017-05-20 09:20:18 -0500

    comentarios al codigo de envio

--- php/procesar-contacto.php
+++ php/procesar-contacto.php
@@ -1,27 +1,21 @@
 <?php
 
-// variable creada con el/los correo de destino
+// destino(s)
 $para = "rodinzon@gmail.com, rodinzon@live.com";
 
-// variables con los datos recibidos del formulario mediante post
+// variables mediante post
 $nombre = $_POST["nombre"] ;
 $telefono = $_POST["telefono"] ;
 $email = $_POST["email"];
 $mensaje = $_POST["mensaje"];
 
-// se crean las cabeceras adicionales
+// cabeceras adicionales
 $cabeceras = 'From: ' . $email ;
 
-// concatenar todos los datos recibidos
+// concatenar datos
 $contenido = "Nombre: " . $nombre . "\nE-mail: " . $email .  "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje ;
 
-
-// Funcion mail que realiza el envio
-// parametro 1: destino
-// parametro 2: asunto
-// parametro 3: mensaje
-// parametro 4: cabecera
-
+// Funcion mail 
 mail ($para, $email, $contenido, $cabeceras);
 
 header("Location:../contacto.php");
