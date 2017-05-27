e649338940dc40d9f01dec2dcc1d9241d724473c
Rodinson Parra <rodinzon@live.com>
2017-05-27 12:13:22 -0500

    cambiados datos de conexion

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php 
 
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "cahouah";
+$db_user = "id1682324_root";
+$db_pass = "Num78Uh%&h";
+$db_name = "id1682324_cahouah";
 $db_table1 = "registros";
 
  ?>
